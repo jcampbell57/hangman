29ba73a5611f105db38a34128419d91d29f20727
jcampbell57 <jcampbell57@gmail.com>
2023-07-20 10:32:51 -0700

    adds methods to load saved game

--- version_2/lib/game.rb
+++ version_2/lib/game.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-# hangman console game game logic
+# hangman console game logic
 class Game
   require 'yaml'
 
   attr_accessor :word_key, :correct_guesses, :incorrect_guesses, :guess_count
 
-  def initialize(incorrect_guesses = [], guess_count = 7, correct_guesses = [], word_key = nil)
-    self.incorrect_guesses = incorrect_guesses
-    self.guess_count = guess_count
-    self.correct_guesses = correct_guesses
-    self.word_key = word_key.nil? ? randomize_key : word_key
-    prompt_guess
+  DEFAULT_GUESSES = 6
+
+  def initialize
+    self.incorrect_guesses = []
+    self.guess_count = DEFAULT_GUESSES
+    self.correct_guesses = []
+    self.word_key = randomize_key
+    start_game
   end
 
   # game initialization methods
@@ -25,31 +27,75 @@ class Game
     word_key
   end
 
+  def start_game
+    puts 'Lets play Hangman in the console!'
+    if Dir.exist?('saves')
+      puts '- [1] for new game'
+      puts '- [2] to load game'
+      print 'New game or saved game: '
+      response = validate_game_selection(gets.chomp.to_i)
+      response == 1 ? prompt_guess : load_game
+    else
+      prompt_guess
+    end
+  end
+
+  def validate_game_selection(input)
+    if input != 1 && input != 2
+      print "Input '1' for new game or '2' for saved game: "
+      validate_game_selection(gets.chomp.to_i)
+    end
+    input
+  end
+
   # game save methods
 
   def save_game
     Dir.mkdir('saves') unless Dir.exist?('saves')
     save_array = [incorrect_guesses, guess_count, correct_guesses, word_key]
-    save_info = YAML.dump(save_array)
     timestamp = Time.new.to_s[0..18]
     filename = "saves/#{timestamp}.yaml"
-    File.open(filename, 'w') do |file|
-      file.puts save_info
-    end
+    File.open(filename, 'w') { |f| YAML.dump([] << save_array, f) }
     puts 'Your game has been saved.'
     exit
   end
 
   def load_game
-    # p YAML.safe_load(save_info)
-    # Game.new(YAML.safe_load(YAML.dump(to_s)))
+    puts '- [#] time when game was saved'
+    saved_games = Dir.glob('saves/*.*')
+    saved_games.each_with_index do |filename, index|
+      puts "- [#{index + 1}] #{filename.delete_prefix('saves/').delete_suffix('.yaml')}"
+    end
+    print 'Enter the # of the game you would like to play: '
+    validate_game_choice(gets.chomp, saved_games)
+  end
+
+  def validate_game_choice(input, saved_games)
+    if input.split('').all? { |character| character.match(/[0-9]/) } && saved_games[input.to_i - 1].nil? == false
+      process_game_choice(input.to_i - 1, saved_games)
+    else
+      print 'Game does not exist, choose again: '
+      validate_game_choice(gets.chomp, saved_games)
+    end
+  end
+
+  def process_game_choice(input, saved_games)
+    yaml = YAML.load_file(saved_games[input])
+    self.incorrect_guesses = yaml[0][0]
+    self.guess_count = yaml[0][1]
+    self.correct_guesses = yaml[0][2]
+    self.word_key = yaml[0][3]
+    File.delete(saved_games[input])
+    prompt_guess
   end
 
   # game loop methods
 
   def prompt_guess
-    puts "Incorrect guesses: #{incorrect_guesses.join(' ')}"
-    puts "Incorrect guesses remaining: #{guess_count}"
+    if guess_count < DEFAULT_GUESSES
+      puts "Incorrect guesses: #{incorrect_guesses.join(' ')}"
+      puts "Incorrect guesses remaining: #{guess_count}"
+    end
     puts "#{correct_guesses.join(' ')}"
     print "Guess a letter or type 'save' to save: "
     validate_guess(gets.chomp)
@@ -90,8 +136,8 @@ class Game
 
   def end_game
     if word_key == correct_guesses
-      # puts "You win with #{guess_count} guesses remaining! Great job!"
-      puts "You won after #{7 - guess_count} incorrect guesses!"
+      puts "You win with #{guess_count} guesses remaining!"
+      # puts "You won after #{DEFAULT_GUESSES - guess_count} incorrect guesses!"
     elsif word_key != correct_guesses && guess_count.zero?
       puts 'You ran out of guesses!'
       puts "The word was: #{word_key.join}"
